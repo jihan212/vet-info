71d71b37404986199e5bb818b593e11250c24cbb
snapper <zihanjashim2121@gmail.comm>
2022-07-07 23:30:48 +0600

    node package updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Homescreen from './Screens/Homescreen'
 import Vetdetailscreen from './Screens/Vetdetailscreen'
 import AboutScreen from './Screens/AboutScreen'
 import APIscreen from './Screens/APIscreen'
+import VetaddScreen from './Screens/VetaddScreen'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path='/details' element={<Vetdetailscreen />} />
           <Route path='/about' element={<AboutScreen />} />
           <Route path='/API' element={<APIscreen />} />
+          <Route path='/Vetadd' element={<VetaddScreen />} />
         </Routes>
       </Router>
     </>
---
+++ frontend/src/Screens/VetaddScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const vetaddScreen = () => {
+    return (
+        <div>
+            <>vet add screen</>
+        </div>
+    );
+};
+
+export default vetaddScreen;
